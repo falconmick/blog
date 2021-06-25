8a57744e90b7d314b01b7d831bbc9f96f7d90217
Michael Crook <ausiemick@gmail.com>
2021-06-26 00:46:37 +0800

    modifed base slug system to mirror blogger

--- sites/blog/gatsby-config.js
+++ sites/blog/gatsby-config.js
@@ -1,4 +1,11 @@
+const { slugify } = require("@michael/gatsby-theme-core/utils");
 require(`dotenv`).config()
+
+const slugWithoutDate = (relativeDirectory) => {
+  const relativeDirectoryWithoutDate = relativeDirectory.replace(/\d{4}-\d{1,2}-\d{1,2}-/, '');
+
+  return slugify(relativeDirectoryWithoutDate);
+};
 
 module.exports = {
   siteMetadata: {
@@ -30,8 +37,14 @@ module.exports = {
   plugins: [
     // This is a list of all themes that this starter is using.
     // To disable a theme, remove it here and run `yarn remove @michael/gatsby-theme-NAME`.
-    `@michael/gatsby-theme-blog-core`,
+    {
+      resolve: `@michael/gatsby-theme-blog-core`,
+      options: {
+        slugResolver: (node, parentNode) => slugWithoutDate(parentNode.relativeDirectory)
+      }
+    },
     `@michael/gatsby-theme-page-core`,
     `@michael/gatsby-theme-phoenix`,
+    `gatsby-plugin-netlify`
   ],
 }
